995f0d52f401968d027d801b32685ae7d0232d2a
inosirov <ibrohim@nosirov.com>
2023-06-01 14:47:55 -0400

    plot convergence rates as a function of q and k for randoming starting.

--- random-starting.jl
+++ random-starting.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using Random
+using Plots
 
 function RandomStarting(A,b,ε,q,k)
     n = size(A, 1)
@@ -32,6 +33,12 @@ function RandomStarting(A,b,ε,q,k)
     return x_approx
 end
 
+function RandomStartingAccuracy(A,b,eps,q,k)
+    x_approx = RandomStarting(A,b,eps,q,k)
+    x_true = A\b
+    return norm(x_true-x_approx)/norm(x_true)
+end
+
 n = 100
 A = randn(n,n)
 A = A*A'
@@ -41,4 +48,30 @@ q = 5
 k = 10
 x_approx = RandomStarting(A,b,ε,q,k)
 x_true = A\b
-norm(x_approx-x_true)/(norm(x_true))
+println(norm(x_approx-x_true)/(norm(x_true)))
+
+n = 100
+A = randn(n, n)
+A = A*A' # otherwise the matrix is too ill-conditioned.
+b = randn(n)
+ε = 0.001
+max_q = 50
+max_k = 50
+
+q_values = 1:max_q
+k_values = 1:max_k
+
+q_accuracies = [RandomStartingAccuracy(A,b,ε,q,10) for q in q_values]
+k_accuracies = [RandomStartingAccuracy(A,b,ε,10,k) for k in k_values]
+
+p1 = plot(q_values, q_accuracies, title="Accuracy vs q", xlab="q",
+ylab="Accuracy", legend=false,linewidth=2,titlefontsize=30, guidefontsize=30,
+tickfontsize=30)
+p2 = plot(k_values, k_accuracies, title="Accuracy vs k", xlab="k",
+ylab="Accuracy", legend=false,linewidth=2,titlefontsize=30, guidefontsize=30,
+tickfontsize=30)
+
+
+plot(p1, p2, layout = (2, 1),size = (2000, 1800))
+
+# how fast is convergence as you increase each one?
